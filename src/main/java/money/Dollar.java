b306ce325bbe386e7fc1d6bb0646537a5feacd50
chikulla <chcklla@gmail.com>
2018-01-14 17:47:29 +0900

    todos

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,7 +1,7 @@
 package money;
 
 public class Dollar {
-    int amount;
+    private int amount;
 
     Dollar(int amount) {
         this.amount = amount;
@@ -10,4 +10,14 @@ public class Dollar {
     Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
     }
+
+    public boolean equals(Object object) {
+        // TODO hate this cast
+        // TODO allow a null argument
+        // TODO allow an another type argument
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
+    }
+
+    // TODO implement hashCode
 }
