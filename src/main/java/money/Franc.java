8de4c672763e41793cfdb930dce80c4389301bf6
chikulla <chcklla@gmail.com>
2018-01-28 15:58:52 +0900

    chapter 7

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,11 +1,11 @@
 package money;
 
-public class Franc extends Money {
+class Franc extends Money {
     Franc(int amount) {
         this.amount = amount;
     }
 
-    Franc times(int multiplier) {
+    Money times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 }
