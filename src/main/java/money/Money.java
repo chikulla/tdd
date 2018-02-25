42cd514afdca66aa5d57d247c1544e8e14c2e7ec
Chikura Hara <chcklla@gmail.com>
2018-02-25 12:27:15 +0900

    chapter 12 & formatted

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,37 +1,41 @@
 package money;
 
-class Money {
-    protected int amount;
-    protected String currency;
+class Money implements Expression {
+  protected int amount;
+  protected String currency;
 
-    Money(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    Money times(int multiplier) {
-        return new Money(this.amount * multiplier, this.currency);
-    }
+  Money times(int multiplier) {
+    return new Money(this.amount * multiplier, this.currency);
+  }
 
-    String currency() {
-        return this.currency;
-    }
+  String currency() {
+    return this.currency;
+  }
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return this.amount == money.amount
-                && this.currency().equals(money.currency());
-    }
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return this.amount == money.amount
+      && this.currency().equals(money.currency());
+  }
 
-    public String toString() {
-        return this.amount + " " + this.currency;
-    }
+  public String toString() {
+    return this.amount + " " + this.currency;
+  }
 
-    static Money dollar(int amount) {
-        return new Money(amount, "USD");
-    }
+  static Money dollar(int amount) {
+    return new Money(amount, "USD");
+  }
 
-    static Money franc(int amount) {
-        return new Money(amount, "CHF");
-    }
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
+  }
+
+  Expression plus(Money addend) {
+    return new Money(this.amount + addend.amount, currency);
+  }
 }
